24a9571d49176ceeeaaafe057fbcac30d560759a
Thomas Aglassinger <roskakori@users.noreply.github.com>
2008-07-14 17:57:46 +0000

    Added empty project with a single class doing nothing so far.

---
+++ source/net/sf/grotas/Grotas.java
@@ -0,0 +1,16 @@
+package net.sf.grotas;
+
+/**
+ * Grotas - AmigaGuide viewer.
+ *
+ * @author Thomas Aglassinger
+ */
+public class Grotas {
+	public Grotas() {
+		
+	}
+
+	public static void main(String[] args) {
+		Grotas grotas = new Grotas();
+	}
+}
