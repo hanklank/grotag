0d054dccb01b61a9502283677a1da37cc338f100
Thomas Aglassinger <roskakori@users.noreply.github.com>
2008-08-05 23:02:41 +0000

    Cleaned up and added some more test cases.

--- source/net/sf/grotag/guide/DocBookWriterTest.java
+++ source/net/sf/grotag/guide/DocBookWriterTest.java
@@ -21,23 +21,36 @@ public class DocBookWriterTest {
         testTools = TestTools.getInstance();
     }
 
-    @Test
-    public void testWriteBasicsGuide() throws IOException, ParserConfigurationException, TransformerException {
-        File inFile = testTools.getTestInputFile("basics.guide");
-        File outFile = testTools.getTestActualFile("basics.xml");
+    private void testWriteGuide(String baseName) throws IOException, ParserConfigurationException, TransformerException {
+        File inFile = testTools.getTestInputFile(baseName + ".guide");
+        File outFile = testTools.getTestActualFile(baseName + ".xml");
         GuidePile pile = new GuidePile();
+        
+        if (!inFile.exists()) {
+            inFile = testTools.getTestGuideFile(baseName + ".guide");
+        }
         pile.addRecursive(inFile);
         DocBookWriter.write(pile, outFile);
         assertTrue(outFile.exists());
     }
 
     @Test
+    public void testWriteBasicsGuide() throws IOException, ParserConfigurationException, TransformerException {
+        testWriteGuide("basics");
+    }
+
+    @Test
     public void testWriteRootGuide() throws IOException, ParserConfigurationException, TransformerException {
-        File inFile = testTools.getTestInputFile("root.guide");
-        File outFile = testTools.getTestActualFile("root.xml");
-        GuidePile pile = new GuidePile();
-        pile.addRecursive(inFile);
-        DocBookWriter.write(pile, outFile);
-        assertTrue(outFile.exists());
+        testWriteGuide("root");
+    }
+
+    @Test
+    public void testWriteAgrGuide() throws IOException, ParserConfigurationException, TransformerException {
+        testWriteGuide("agr");
+    }
+
+    @Test
+    public void testWriteAgrTestGuide() throws IOException, ParserConfigurationException, TransformerException {
+        testWriteGuide("agr_test");
     }
 }
