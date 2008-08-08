fd882528f243e85d604e4ad1a92fc51f5533b014
Thomas Aglassinger <roskakori@users.noreply.github.com>
2008-08-08 19:20:24 +0000

    - Changed command line parsing to use JSAP, available from <http://www.martiansoftware.com/jsap/>. - Changed --pretty to allow multiple files and automatically overwrite the source file. - Changed --validate to allow multiple files. - Added command line options --license and --version.

--- source/net/sf/grotag/Grotag.java
+++ source/net/sf/grotag/Grotag.java
@@ -1,64 +1,127 @@
 package net.sf.grotag;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import net.sf.grotag.common.Tools;
 import net.sf.grotag.guide.DocBookWriter;
 import net.sf.grotag.guide.Guide;
 import net.sf.grotag.guide.GuidePile;
 
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+
 /**
- * Grotag - Amigaguide viewer and converter.
+ * Grotag - Amigaguide converter and pretty printer.
  * 
  * @author Thomas Aglassinger
  */
 public class Grotag {
-    enum Action {
-        DOCBOOK, HELP, PRETTY, VALIDATE
+    private GrotagJsap jsap;
+
+    private Grotag() throws JSAPException {
+        jsap = new GrotagJsap();
     }
 
-    public static void main(String[] args) throws Exception {
-        Action action = Action.HELP;
-        String sourceFilePath = null;
-        String targetFilePath = null;
+    private void work(String[] arguments) throws IOException, ParserConfigurationException, TransformerException {
+        JSAPResult options = jsap.parse(arguments);
 
-        for (String option : args) {
-            if (option.startsWith("-")) {
-                option = option.substring(1);
-                action = Action.valueOf(option.toUpperCase());
-            } else if (sourceFilePath == null) {
-                sourceFilePath = option;
-            } else if (targetFilePath == null) {
-                targetFilePath = option;
+        if (!options.success()) {
+            // Throw exception for broken command line.
+            Iterator<String> errorRider = options.getErrorMessageIterator();
+            String errorMessage;
+
+            if (errorRider.hasNext()) {
+                errorMessage = errorRider.next();
             } else {
-                throw new IllegalArgumentException("cannot process additional option: " + option);
+                errorMessage = null;
             }
-        }
-        if ((sourceFilePath == null)
-                && ((action == Action.DOCBOOK) || (action == Action.PRETTY) || (action == Action.VALIDATE))) {
-            throw new IllegalArgumentException("source file must be specified");
-        }
-        if ((targetFilePath == null) && ((action == Action.DOCBOOK) || (action == Action.PRETTY))) {
-            throw new IllegalArgumentException("target file must be specified");
+            throw new IllegalArgumentException(errorMessage);
         }
 
-        if (action == Action.HELP) {
-            System.err.println("Usage: java -jar Grotag.jar -docbook|-pretty|-validate source_file [target_file]");
-        } else if (action == Action.PRETTY) {
-            File sourceFile = new File(sourceFilePath);
-            File targetFile = new File(targetFilePath);
-            Guide guide = Guide.createGuide(sourceFile);
-            guide.writePretty(targetFile);
-        } else {
-            File sourceFile = new File(sourceFilePath);
-            GuidePile pile = GuidePile.createGuidePile(sourceFile);
-            if (action != Action.VALIDATE) {
-                File targetFile = new File(targetFilePath);
-                if (action == Action.DOCBOOK) {
-                    DocBookWriter.write(pile, targetFile);
-                } else {
-                    assert false : "action=" + action;
-                }
+        boolean isDocBook = options.getBoolean(GrotagJsap.ARG_DOCBOOK);
+        boolean isPretty = options.getBoolean(GrotagJsap.ARG_PRETTY);
+        boolean isValidate = options.getBoolean(GrotagJsap.ARG_VALIDATE);
+        if (isDocBook || isPretty || isValidate) {
+            File files[] = options.getFileArray(GrotagJsap.ARG_FILE);
+            // According to JSAP API documentation, this is never is null.
+            assert files != null;
+            if (files.length == 0) {
+                throw new IllegalArgumentException("Amigaguide input file must be specified");
             }
+            if (isDocBook) {
+                docBook(files);
+            } else if (isPretty) {
+                pretty(files);
+            } else if (isValidate) {
+                validate(files);
+            } else {
+                assert false;
+            }
+        } else if (options.getBoolean(GrotagJsap.ARG_HELP)) {
+            jsap.printHelp(System.err);
+        } else if (options.getBoolean(GrotagJsap.ARG_LICENSE)) {
+            jsap.printLicense(System.out);
+        } else if (options.getBoolean(GrotagJsap.ARG_VERSION)) {
+            jsap.printVersion(System.out);
+        } else {
+            assert false : "GUI must be implemented";
         }
+    }
+
+    private void docBook(File[] files) throws IOException, ParserConfigurationException, TransformerException {
+        int fileCount = files.length;
+        File inputFile;
+        File outputFile;
+        if (fileCount == 0) {
+            throw new IllegalArgumentException("Amigaguide input file must be specified");
+        } else if (fileCount == 1) {
+            inputFile = files[0];
+            File outputFileFolder = inputFile.getParentFile();
+            String inputFileName = inputFile.getName();
+            String outputFileName = Tools.getInstance().getWithoutLastSuffix(inputFileName) + ".xml";
+            outputFile = new File(outputFileFolder, outputFileName);
+        } else if (fileCount == 2) {
+            inputFile = files[0];
+            outputFile = files[1];
+        } else {
+            throw new IllegalArgumentException("only 2 files must be specified instead of " + fileCount);
+        }
+        GuidePile pile = GuidePile.createGuidePile(inputFile);
+        DocBookWriter.write(pile, outputFile);
+    }
+
+    private void pretty(File[] files) throws IOException {
+        for (File guideFile : files) {
+            Guide guide = Guide.createGuide(guideFile);
+            guide.writePretty(guideFile);
+        }
+    }
+
+    private void validate(File[] files) throws IOException {
+        for (File guideFile : files) {
+            GuidePile.createGuidePile(guideFile);
+        }
+    }
+
+    public static void main(final String[] arguments) {
+        Logger mainLog = Logger.getLogger(Grotag.class.getName());
+        int exitCode = 1;
+        try {
+            Grotag grotag = new Grotag();
+            grotag.work(arguments);
+            exitCode = 0;
+        } catch (IllegalArgumentException error) {
+            mainLog.log(Level.INFO, "cannot process command line options: " + error.getMessage(), error);
+        } catch (Throwable error) {
+            mainLog.log(Level.SEVERE, "cannot run Grotag" + error.getMessage(), error);
+        }
+        System.exit(exitCode);
     }
 }
---
+++ source/net/sf/grotag/GrotagJsap.java
@@ -0,0 +1,146 @@
+package net.sf.grotag;
+
+import java.io.PrintStream;
+
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Switch;
+import com.martiansoftware.jsap.SyntaxException;
+import com.martiansoftware.jsap.UnflaggedOption;
+import com.martiansoftware.jsap.stringparsers.FileStringParser;
+
+public class GrotagJsap extends JSAP {
+    public static final String ARG_DOCBOOK = "docbook";
+    public static final String ARG_FILE = "file";
+    public static final String ARG_HELP = "help";
+    public static final String ARG_LICENSE = "license";
+    public static final String ARG_PRETTY = "pretty";
+    public static final String ARG_VALIDATE = "validate";
+    public static final String ARG_VERSION = "version";
+
+    /**
+     * Exactly one of these options must be specified.
+     */
+    private static final String[] ANY_REQUIRED = new String[] { ARG_DOCBOOK, ARG_HELP, ARG_LICENSE, ARG_PRETTY,
+            ARG_VERSION, ARG_VALIDATE };
+
+    // TODO: Obtain copyright year from Version class.
+    private static final String[] HEADING = new String[] { "Grotag - Amigaguide converter and pretty printer.",
+            "Copyright (C) 2008  Thomas Aglassinger <http://grotag.sourceforge.net/>" };
+
+    private static final String[] LICENSE = new String[] { "",
+            "This program is free software: you can redistribute it and/or modify",
+            "it under the terms of the GNU General Public License as published by",
+            "the Free Software Foundation, either version 3 of the License, or", "(at your option) any later version.",
+            "", "This program is distributed in the hope that it will be useful,",
+            "but WITHOUT ANY WARRANTY; without even the implied warranty of",
+            "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+            "GNU General Public License for more details.", "",
+            "You should have received a copy of the GNU General Public License",
+            "along with this program.  If not, see <http://www.gnu.org/licenses/>." };
+
+    /**
+     * Create JSAP for Grotag command line options.
+     */
+    public GrotagJsap() throws JSAPException {
+        UnflaggedOption fileOption = new UnflaggedOption(ARG_FILE).setStringParser(FileStringParser.getParser());
+        fileOption.setHelp("the files to be processed; when converting, the last file is the target file");
+        registerParameter(fileOption);
+
+        Switch docBookSwitch = new Switch(ARG_DOCBOOK).setShortFlag('d').setLongFlag(ARG_DOCBOOK);
+        docBookSwitch.setHelp("convert to DocBook XML");
+        registerParameter(docBookSwitch);
+
+        Switch prettySwitch = new Switch(ARG_PRETTY).setShortFlag('v').setLongFlag(ARG_PRETTY);
+        prettySwitch.setHelp("cleanup specified Amigaguide documents, overwritting the original");
+        registerParameter(prettySwitch);
+
+        Switch validateSwitch = new Switch(ARG_VALIDATE).setShortFlag('p').setLongFlag(ARG_VALIDATE);
+        validateSwitch.setHelp("validate specified Amigaguide documents");
+        registerParameter(validateSwitch);
+
+        Switch helpSwitch = new Switch(ARG_HELP).setShortFlag('h').setLongFlag(ARG_HELP);
+        helpSwitch.setHelp("show usage instructions");
+        registerParameter(helpSwitch);
+
+        Switch licenseSwitch = new Switch(ARG_LICENSE).setShortFlag(NO_SHORTFLAG).setLongFlag(ARG_LICENSE);
+        licenseSwitch.setHelp("show license");
+        registerParameter(licenseSwitch);
+
+        Switch versionSwitch = new Switch(ARG_VERSION).setShortFlag(NO_SHORTFLAG).setLongFlag(ARG_VERSION);
+        versionSwitch.setHelp("show version information");
+        registerParameter(versionSwitch);
+    }
+
+    @Override
+    public JSAPResult parse(String argument) {
+        JSAPResult result = super.parse(argument);
+
+        parseAnyRequired(result);
+        return result;
+    }
+
+    @Override
+    public JSAPResult parse(String[] arguments) {
+        JSAPResult result = super.parse(arguments);
+
+        parseAnyRequired(result);
+        return result;
+    }
+
+    /**
+     * Print help about command line options.
+     */
+    public void printHelp(PrintStream stream) {
+        assert stream != null;
+        stream.println(HEADING);
+        stream.println();
+        stream.println(getHelp());
+    }
+
+    /**
+     * Print summary of license.
+     */
+    public void printLicense(PrintStream stream) {
+        assert stream != null;
+        for (String line : LICENSE) {
+            stream.println(line);
+        }
+    }
+
+    /**
+     * Print version number.
+     */
+    public void printVersion(PrintStream stream) {
+        // TODO: Obtain version number from Version class.
+        stream.println("Grotag 0.1");
+    }
+
+    private void parseAnyRequired(JSAPResult options) {
+        assert options != null;
+        String commandOption = null;
+
+        for (int i = 0; i < ANY_REQUIRED.length; i += 1) {
+            String nextOption = ANY_REQUIRED[i];
+            Object nextValue = options.getObject(nextOption);
+            boolean hasValue = (nextValue != null);
+
+            if (hasValue) {
+                if (nextValue instanceof Boolean) {
+                    hasValue = ((Boolean) nextValue).booleanValue();
+                }
+            }
+            if (hasValue) {
+                if (commandOption == null) {
+                    commandOption = nextOption;
+                } else {
+                    SyntaxException error = new SyntaxException("option \"" + nextOption
+                            + "\" must not be used together with \"" + commandOption + "\"");
+
+                    options.addException(nextOption, error);
+                }
+            }
+        }
+    }
+}
