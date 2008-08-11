a2d756973116eab0c36729e2b980a65e6cfd4c54
Thomas Aglassinger <roskakori@users.noreply.github.com>
2008-08-11 22:02:29 +0000

    Added twelvemonkeys-imageio available from <https://twelvemonkeys-imageio.dev.java.net/>. This will turn out useful for displaying IFF/ILBM pictures.

---
+++ tests/net/sf/grotag/IlbmImageTest.java
@@ -0,0 +1,54 @@
+package net.sf.grotag;
+
+import static org.junit.Assert.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.sf.grotag.common.TestTools;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Testcase for IFFReader (just to see how it works).
+ * 
+ * @author Thomas Aglassinger
+ */
+public class IlbmImageTest {
+    private static final boolean SHOW_AMIGABALL = false;
+    private TestTools testTools;
+
+    @Before
+    public void setUp() throws Exception {
+        testTools = TestTools.getInstance();
+    }
+
+    @Test
+    public void testReadIlbmImage() throws IOException {
+        File ilbmFile = testTools.getTestInputFile("amigaball.ilbm");
+        BufferedImage amigaBall = ImageIO.read(ilbmFile);
+        assertNotNull(amigaBall);
+
+        if (SHOW_AMIGABALL) {
+            JPanel pane = new JPanel();
+            pane.add(new JButton(new ImageIcon(amigaBall)));
+            JFrame frame = new JFrame(IlbmImageTest.class.getName());
+            frame.setContentPane(pane);
+            frame.pack();
+            frame.setVisible(true);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException error) {
+                error.printStackTrace();
+            }
+        }
+    }
+}
