b7a99da818e8ae7a6bbc47041044801631b5f01c
Thomas Aglassinger <roskakori@users.noreply.github.com>
2008-09-18 20:58:48 +0000

    Added simple "About" window.

--- source/net/sf/grotag/MacGrotag.java
+++ source/net/sf/grotag/MacGrotag.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 
 import net.roydesign.app.Application;
 import net.roydesign.event.ApplicationEvent;
+import net.roydesign.ui.StandardMacAboutFrame;
 import net.sf.grotag.common.Tools;
 import net.sf.grotag.view.GrotagFrame;
 
@@ -19,6 +20,8 @@ public class MacGrotag implements ActionListener {
     private Application application;
     private GrotagFrame frame;
     private Logger log;
+    private StandardMacAboutFrame aboutFrame;
+    private String aboutFrameLock;
 
     public MacGrotag() {
         // Setup logging.
@@ -27,8 +30,10 @@ public class MacGrotag implements ActionListener {
         log = Logger.getLogger(MacGrotag.class.getName());
         frame = new GrotagFrame();
         frame.setVisible(true);
+        aboutFrameLock = "x";
         application = Application.getInstance();
         application.addOpenDocumentListener(this);
+        application.getAboutJMenuItem().addActionListener(this);
     }
 
     public static void main(String[] arguments) {
@@ -39,7 +44,18 @@ public class MacGrotag implements ActionListener {
         try {
             ApplicationEvent event = (ApplicationEvent) actionEvent;
             int type = event.getType();
-            if (type == ApplicationEvent.OPEN_DOCUMENT) {
+            if (type == ApplicationEvent.ABOUT) {
+                log.info("handle \"about\" event");
+                synchronized (aboutFrameLock) {
+                    if (aboutFrame == null) {
+                        // FIXME: Use Version class.
+                        aboutFrame = new StandardMacAboutFrame("Grotag", "0.1.0");
+                        aboutFrame.setCopyright("Copyright 2008 Thomas Aglassinger.");
+                        aboutFrame.pack();
+                    }
+                    aboutFrame.setVisible(true);
+                }
+            } else if (type == ApplicationEvent.OPEN_DOCUMENT) {
                 log.info("handle \"open document\" event");
                 frame.read(event.getFile());
             } else {
